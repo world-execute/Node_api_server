4575d599ad70d429b11f4e975a86e0773d3713f1
菍磐 <qaz123cxd@yeah.net>
2022-11-10 16:06:28 +0800

    commit

--- db/index.js
+++ db/index.js
@@ -1,8 +1,13 @@
 const mysql = require('mysql')
+// 创建连接池
 const db = mysql.createPool({
+    // 主机地址
     host:'localhost',
+    // 用户名
     user:'root',
+    // 密码
     password:'622821',
+    // 数据库
     database:'test'
 })
 module.exports = db
